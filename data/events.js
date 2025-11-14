0a11f1c371df02e769382cf757fa179a040e9352
Aydeth <tumyrbaranovski@gmail.com>
2025-11-14 04:30:19 +0300

    last one for today

---
+++ data/events.js
@@ -0,0 +1,79 @@
+const eventsData = [
+    {
+        id: 1,
+        name: "Target Extraction - The 7th Walpurgis Night",
+        image: "img/events/2025/wpn7.png",
+        endDate: "2025-11-20T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Target Extraction - The 2nd+3rd Walpurgis Night BokGak",
+        image: "img/events/2025/wpn23.png",
+        endDate: "2025-11-20T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "A Certain Library's General Invitation - Enkephalin Event",
+        image: "img/events/2025/wpn7e.png",
+        endDate: "2025-11-20T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 4,
+        name: "Murder on the WARP Express BokGak",
+        image: "img/events/2025/warpbg.png",
+        endDate: "2025-11-20T01:00:00Z",
+        status: "active"
+    },
+    /*{
+        id: 0,
+        name: "Murder on the WARP Express BokGak",
+        image: "img/events/2025/warpbg.png",
+        endDate: "2025-11-20T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 0,
+        name: "Target Extraction - Murder on the WARP Express BokGak",
+        image: "img/events/2025/te-warp.png",
+        endDate: "2025-11-06T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 0,
+        name: "Target Extraction - WARP Corporation",
+        image: "img/events/2025/te-wcorp.png",
+        endDate: "2025-11-06T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 0,
+        name: "Target Extraction - Shi Assoc. East Section 3 Faust",
+        image: "img/events/2025/te-shi.png",
+        endDate: "2025-10-23T01:00:00Z",
+        status: "active"
+    },
+    {
+        id: 0,
+        name: "Target Extraction - Gregor",
+        image: "img/events/2025/te-gr1.png",
+        endDate: "2025-10-23T01:00:00Z",
+        status: "active"
+    },*/
+    {
+        id: 6,
+        name: "Line 5: Merry Go Round",
+        image: "img/events/2025/rr5.png",
+        endDate: "2025-09-09T00:00:00Z",
+        status: "na"
+    },
+    {
+        id: 7,
+        name: "Season 6: Zàng Huā Yín",
+        image: "img/events/2025/s6.png",
+        endDate: "2025-09-09T00:00:00Z",
+        status: "na"
+    }
+];
